d67642ca12e5599b5d035b0f16320921929871f0
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2023-02-04 17:39:04 -0600

    Changed website name to "Endless Dress" and add controllers folder with product controllers

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-3'>
-            Copyright &copy; Dress for less
+            Copyright &copy; Endless Dress
           </Col>
         </Row>
       </Container>
