a4fbc83d6c2dd80f9771742a838493e9f57a10b0
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2023-02-20 14:35:59 -0600

    Added register functionality, but need to fix postman

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,8 @@ import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import ProductDisplayPage from './pages/ProductDisplayPage'
 import CartPage from './pages/CartPage'
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
             <Route path='/product/:id' element={<ProductDisplayPage />} />
             <Route path='/cart/:id?' element={<CartPage />} />
             <Route path='/' element={<HomePage />} exact />
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -5,18 +5,21 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { login } from '../actions/userActions'
+import { register } from '../actions/userActions'
 
-const LoginPage = ({  }) => {
+const RegisterPage = ({  }) => {
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState()
 
     const location = useLocation()
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
-    const userLogin = useSelector(state => state.userLogin)
-    const { loading, error, userInfo } = userLogin
+    const userRegister = useSelector(state => state.userRegister)
+    const { loading, error, userInfo } = userRegister
 
     const redirect = location.search ? location.search.split('=')[1] : '/'
 
@@ -28,14 +31,29 @@ const LoginPage = ({  }) => {
 
     const submitHandler = (e) => {
         e.preventDefault()
-        dispatch(login(email, password))
+        if(password !== confirmPassword){
+            setMessage('Passwords do not match')
+        } else{
+            dispatch(register(name, email, password))
+        }
     }
   
     return <FormContainer>
-        <h1>Sign In</h1>
+        <h1>Sign Up</h1>
+        {message && <Message variant='danger'>{message}</Message>}
         {error && <Message variant='danger'>{error}</Message>}
         {loading && <Loader />}
         <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control 
+                    type='name' 
+                    placeholder='Enter name' 
+                    value={name} 
+                    onChange={(e) => setName(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+
             <Form.Group controlId='email'>
                 <Form.Label>Email Address</Form.Label>
                 <Form.Control 
@@ -54,18 +72,28 @@ const LoginPage = ({  }) => {
                     value={password} 
                     onChange={(e) => setPassword(e.target.value)}
                 ></Form.Control>
-
-                <Button type='submit' variant='primary'>
-                    Sign In
-                </Button>
             </Form.Group>
+
+            <Form.Group controlId='confirmPassword'>
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control 
+                    type='password' 
+                    placeholder='Confirm password' 
+                    value={confirmPassword} 
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+                Register
+            </Button>
         </Form>
         
         <Row className='py-3'>
             <Col>
-                New User?{' '}
-                <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-                    Register
+                Have an Account?{' '}
+                <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+                    Login
                 </Link>
             </Col>
         </Row>
@@ -73,4 +101,4 @@ const LoginPage = ({  }) => {
   
 }
 
-export default LoginPage
+export default RegisterPage
