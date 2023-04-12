b69a5b12ea22b9336758d3b8a066e8355fb35870
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2023-04-12 00:45:42 -0500

    User list page is complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -123,7 +123,45 @@ const deleteUser = asyncHandler(async(req, res) => {
         res.json({ message: 'User removed' })
     } else {
         res.status(404)
-        throw new Error('User not founds')
+        throw new Error('User not found')
+    }
+})
+
+// description: Get user by id
+// route: POST /api/users/:id
+// access: Private/Admin
+const getUserById = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+
+    if(user){
+        res.json(user)
+    } else {
+        throw new Error('User not found')
+    }
+})
+
+// description: Update user
+// route: PUT /api/users/:id
+// access: Private/Admin
+const updateUser = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.params.id)
+
+    if(user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        user.isAdmin = req.body.isAdmin
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        })
+    } else{
+        res.status(404)
+        throw new Error('User not found')
     }
 })
 
@@ -134,4 +172,6 @@ export{
     updateUserProfile,
     getUsers,
     deleteUser,
+    getUserById,
+    updateUser,
 }
