dc2eafd9ecb968327e8bb7670f20e1b93f7ac9b3
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2025-10-29 03:48:05 -0500

    getApiUrlpiUrl

--- frontend/src/pages/OrderConfirmationPage.js
+++ frontend/src/pages/OrderConfirmationPage.js
@@ -38,7 +38,7 @@ const OrderConfirmationPage = ({ match }) => {
         }
         const addPayPalScript = async () => {
             try {
-                const  { data: clientId } = await axios.get(getApiUrlpiUrl(`/api/config/paypal`))
+                const  { data: clientId } = await axios.get(getApiUrl(`/api/config/paypal`))
                 const script = document.createElement('script')
                 script.type = 'text/javascript'
                 script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}&disable-funding=credit,card`
